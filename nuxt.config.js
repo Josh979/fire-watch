5f3b838b2bf7bdd1edf172f66ad23b8914b17b0f
Josh979 <jbmielke@gmail.com>
2020-09-17 14:36:35 -0700

    Updated Page Title, added rel="noopener" to external links, added title tags to links

--- nuxt.config.js
+++ nuxt.config.js
@@ -10,7 +10,7 @@ export default {
   ** See https://nuxtjs.org/api/configuration-head
   */
   head: {
-    title: process.env.npm_package_name || '',
+    title: 'California Fire Watch' || '',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },

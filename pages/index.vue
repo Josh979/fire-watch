05390db8576fd5e808310162139ef7e812836729
Josh979 <jbmielke@gmail.com>
2020-09-18 16:49:38 -0700

    Added sorting/filter functionality

--- pages/index.vue
+++ pages/index.vue
@@ -6,9 +6,28 @@
       </div>
     </nav>
 
-    <div class="container mx-auto mt-5 " v-if="fires">
+    <div class="container mx-auto mt-5 " v-if="filteredFires">
+      <div class="flex justify-end">
+        <div class="ml-2">
+          <label class="block text-sm">Sort By</label>
+          <select class="px-3 py-2 rounded shadow-sm text-gray-900 bg-white border-solid border-2 border-gray-300" v-model="filter">
+            <option value="Name">Name</option>
+            <option value="AcresBurned">Acres Burned</option>
+            <option value="PercentContained">Percent Contained</option>
+          </select>
+        </div>
+        <div class="ml-2">
+          <label class="block text-sm">Order</label>
+          <select class="px-3 py-2 rounded shadow-sm text-gray-900 bg-white border-solid border-2 border-gray-300" v-model="ascending">
+            <option :value="1">Ascending</option>
+            <option :value="0">Descending</option>
+          </select>
+        </div>
+      </div>
+
+
       <div
-        v-for="(fire,index) in fires"
+        v-for="(fire,index) in filteredFires"
         v-if="fire.AcresBurned > 0"
         :key="index"
         class=" flex rounded overflow-hidden shadow-md my-5 bg-white"
@@ -61,9 +80,12 @@
 
 <script>
 export default {
+  name: 'Home',
   data: function () {
     return {
       fires: null,
+      filter: 'Name',
+      ascending: 1
     }
   },
   methods: {
@@ -82,10 +104,25 @@ export default {
   async created() {
     await this.fetchFires();
   },
+  computed: {
+    filteredFires() {
+      if (this.fires !== null){
+        return this.fires.sort((a,b) => {
+          let order = 1;
+          if (a[this.filter] > b[this.filter]){
+            order = 1;
+          } else if (a[this.filter] < b[this.filter]){
+            order = -1;
+          }
+          return order * ((this.ascending) ? 1 : -1);
+        })
+      }
+      return null;
+    }
+  }
 
 }
 </script>
 
 <style>
-
 </style>
